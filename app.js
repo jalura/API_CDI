da0f611ec12b85b66eff1cca4cbc789e69bdf003
mente <mente63@gmail.com>
2021-06-18 13:01:03 -0500

    Integracion consulta por skill v.01

--- app.js
+++ app.js
@@ -881,24 +881,9 @@ app.get('/OOADProblematicaSkill/:cveSkill', (peticion, respuesta) => {
                     }
                   }
                   var nombreUsuario = "";
-                  var sqlUsuario = `select NOM_NOMBRE, NOM_APELLIDOPATERNO,  NOM_APELLIDOMATERNO from SIAT_USUARIO where CVE_CORREO = '` + cveCorreo + `'`;
-                  imprimeTRACE.logOperacion('Desc: Obtiene Nombre Usuario a partir de cve Skill(IMSS-CDI)', sqlUsuario, nivelTRACE);
-                  conexionBBDD.query(sqlUsuario, (error, resultado)=>{
-                    if (error) {
-                        console.log("ERROR AL ACCESAR LA BBDD PARA OBTENER EL NOMBRE DEL USUARIO");
-                        nombreUsuario = cveCorreo;
-                    } else {
-                        console.log("SE OBTIENE EL NOMBRE DEL USUARIO");
-                        console.log(JSON.stringify(resultado, null, '-'));
-                        var nameUser = JSON.stringify(resultado);
-                        console.log("PASO JSON.stringify");
-                        nameUser = JSON.parse(nameUser);    
-                        console.log("PASO JSON.parse");
-                        console.log(nameUser);
-                        nombreUsuario = nameUser[0].NOM_NOMBRE + " " + nameUser[0].NOM_APELLIDOPATERNO + " " + nameUser[0].NOM_APELLIDOMATERNO; 
-                        console.log("NOMBRE USUARIO: " + nombreUsuario);    
-                    }
-                  });
+
+
+                  
                   var sql = 'select op.CVE_OOAD_PROBLEMATICA, op.NOM_RESPONSABLE, op.DES_OTRO, so.NOM_NOMBRE OOAD_NOMBRE, ss.NOM_NOMBRE STATUS, ';
                   sql = sql + "sp.NOM_NOMBRE PROBLEMATICA_NOMBRE , sn.NOM_NOMBRE NIVEL, DATE_FORMAT(op.FEC_ALTA, '%Y-%m-%d') FEC_ALTA, "
                   sql = sql + "'" + cveSkill + "' as SKILL, '" + nombreUsuario + "' as USUARIO ";
