dd05e6097218581197a04e4b9c1cdaab19f88d63
mente <mente63@gmail.com>
2021-06-18 00:10:40 -0500

    Integracion consulta por skill v.01

--- app.js
+++ app.js
@@ -894,7 +894,7 @@ app.get('/OOADProblematicaSkill/:cveSkill', (peticion, respuesta) => {
             console.log('**************************');
             console.Console("Subtipos: " + subTipos);   
             var sql = 'select op.CVE_OOAD_PROBLEMATICA, op.NOM_RESPONSABLE, op.DES_OTRO, so.NOM_NOMBRE OOAD_NOMBRE, ss.NOM_NOMBRE STATUS, ';
-            sql = sql + 'sp.NOM_NOMBRE PROBLEMATICA_NOMBRE , sn.NOM_NOMBRE NIVEL, DATE_FORMAT(op.FEC_ALTA, '%Y-%m-%d') FEC_ALTA  ';
+            sql = sql + "sp.NOM_NOMBRE PROBLEMATICA_NOMBRE , sn.NOM_NOMBRE NIVEL, DATE_FORMAT(op.FEC_ALTA, '%Y-%m-%d') FEC_ALTA  ";
             sql = sql + 'FROM  SIAC_OOAD_PROBLEMATICA op ';
             sql = sql + 'JOIN  SIAC_OOAD so USING(CVE_OOAD) ';
             sql = sql + 'JOIN  SIAC_PROBLEMATICA sp USING(CVE_PROBLEMATICA) '; 
